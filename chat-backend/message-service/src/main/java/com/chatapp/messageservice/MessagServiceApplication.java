c4e7176f99f0070a36161bfd903f068d0bf0c944
NguyenBaoZ <ngiabao508@gmail.com>
2025-10-18 15:17:08 +0700

    add message service

---
+++ chat-backend/message-service/src/main/java/com/chatapp/messageservice/MessagServiceApplication.java
@@ -0,0 +1,11 @@
+package com.chatapp.messageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessagServiceApplication.class, args);
+    }
+}
